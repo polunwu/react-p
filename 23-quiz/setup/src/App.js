cd8ace886cce2730fd821e28e1d62f4e690da7c9
polunwu <polunwu@gmail.com>
2021-05-15 14:59:43 +0800

    Complete 23-quiz

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -5,7 +5,61 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  return <h2>quiz starter</h2>
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext()
+
+  if (waiting) {
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
+
+  const { question, correct_answer, incorrect_answers } = questions[index]
+  // const answers = [...incorrect_answers, correct_answer]
+  let answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random() * 4)
+  if (tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
+  return (
+    <main>
+      <Modal />
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers: {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  onClick={() => checkAnswer(answer === correct_answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                ></button>
+              )
+            })}
+          </div>
+        </article>
+        <button onClick={nextQuestion} className="next-question">
+          next question
+        </button>
+      </section>
+    </main>
+  )
 }
 
 export default App
